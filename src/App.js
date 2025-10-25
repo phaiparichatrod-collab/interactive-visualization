6b891600f16d566ddb1b05cf987c66cf17e63bae
eyenisss <eyenissaa@gmail.com>
2025-10-25 16:23:02 +0700

    App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
 import VoteEventList from './services/voteEventList';
+import GraphView from './components/GraphView';
 import client from './graphql/apollo-client';
 import { ApolloProvider } from '@apollo/client/react';
-
 
 function App() {
   return (
     <ApolloProvider client={client}>
       <div style={{ fontFamily: 'Arial, sans-serif', padding: '20px' }}>
         <h1>Interactive Political Visualization</h1>
-        <p>Connected to GraphQL API: <a href="https://politigraph.wevis.info/" target="_blank">https://politigraph.wevis.info/</a></p>
+        <p>
+          Connected to GraphQL API:{' '}
+          <a href="https://politigraph.wevis.info/" target="_blank">
+            https://politigraph.wevis.info/
+          </a>
+        </p>
         <VoteEventList />
+        <GraphView />
       </div>
     </ApolloProvider>
   );
