0b6107096c0cd8d8cea5fde8781242f6bb1144b5
eyenisss <eyenissaa@gmail.com>
2025-10-25 22:36:04 +0700

    apollo-client.js

--- src/graphql/apollo-client.js
+++ src/graphql/apollo-client.js
@@ -1,24 +1,20 @@
 import { ApolloClient, InMemoryCache, HttpLink, from } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
-// 1) ตั้งค่า HTTP Link (API endpoint)
+// เชื่อมต่อ GraphQL API
 const httpLink = new HttpLink({
-  uri: "https://politigraph.wevis.info/v1/graphql", // เปลี่ยนตามที่โปรเจคใช้
+  uri: "https://politigraph.wevis.info/graphql",
 });
 
-// 2) (ถ้าต้องส่ง Token) ทำ Auth Link
-// ถ้าไม่ใช้ Token ปล่อยให้ authLink เป็น passthrough ได้
+// (ถ้าไม่มี token ก็ไม่ต้องแก้)
 const authLink = setContext((_, { headers }) => {
-  const token = ""; // <= ใส่ token ถ้ามี
   return {
     headers: {
       ...headers,
-      ...(token && { Authorization: `Bearer ${token}` }),
     },
   };
 });
 
-// 3) สร้าง Apollo Client + enable cache
 const client = new ApolloClient({
   link: from([authLink, httpLink]),
   cache: new InMemoryCache(),
